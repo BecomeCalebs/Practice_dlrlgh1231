71f1138fbc82cecbd78ac212db80b055cdd09628
TheSolid-KANG <tortoise8947@gmail.com>
2023-06-12 23:35:21 +0900

    no message

---
+++ src/CPractice/CPractice.java
@@ -0,0 +1,20 @@
+package CPractice;
+
+public class CPractice{
+    //필드(=멤버변수) 초기화
+    public void initialize(){
+        
+    } 
+    // 필드(=멤버변수) 값 변경 
+    public void update(){
+
+    } 
+    // 화면에 인스턴스 정보 출력
+    public void render(){
+
+    } 
+    // 
+    public void release(){
+        
+    } 
+}
---
+++ src/main/App.java
@@ -0,0 +1,27 @@
+package main;
+
+import CPractice.CPractice;
+import CPractice.CPractice_1.CPractice_1;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        //확인
+        //1. CPractice_1 인스턴스 생성 부분
+        // * 인스턴스: 자료형의 데이터가 메모리의 생성된 것.
+        //2. 형변환: CPractice 변수에 CPractice_1의 인스턴스를 담을 수 있다.
+        //암묵적 형변환
+        CPractice practice = new CPractice_1(); //자료형이 CPractice(=부모클래스)이기 때문에 CPractice가 갖고 있는 필드 or 메서드만 사용 가능하다.
+        //부모 = new 자식();
+
+        //override되었으므로 자식클래스에서 정의한 메서드가 호출됨.
+        practice.render();
+    }
+}
+
+
+/*
+안녕하세요.
+이기호 형제입니다. 
+잘부탁드립니다.
+*/ 
